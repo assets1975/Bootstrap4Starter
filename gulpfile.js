67a1593b6ba5245ac08c8893454bde3203cb2a46
Asset Saparov <asset_s@mail.ru>
2018-06-05 16:50:20 +0600

    add gulp-eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,80 +1,89 @@
-const gulp        = require('gulp');
-//const browserSync = require('browser-sync').create();
-const browserSync = require("browser-sync");
-const sass        = require('gulp-sass');
-
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
 const prefixer = require('gulp-autoprefixer');
 const cssmin = require('gulp-minify-css');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
-const cleanCSS = require('gulp-clean-css');
+// const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
 const rimraf = require('rimraf');
+const eslint = require('gulp-eslint');
 const reload = browserSync.reload;
 
 // Compile Sass & Inject Into Browser
 gulp.task('scss:build', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss']) // Выбираем наши scss фаилы
-    //return gulp.src([ 'src/scss/*.scss']) // Выбираем наши scss фаилы
-        .pipe(sourcemaps.init()) //То же самое что и с js
-        .pipe(sass()) //Скомпилируем
-        .pipe(prefixer()) //Добавим вендорные префиксы
-        .pipe(cssmin()) //Сожмем
+    return gulp.src([
+        'node_modules/bootstrap/scss/bootstrap.scss',
+        'src/scss/*.scss'
+    ]) // Выбираем наши scss фаилы
+    //  return gulp.src([ 'src/scss/*.scss']) // Выбираем наши scss фаилы
+        .pipe(sourcemaps.init()) // То же самое что и с js
+        .pipe(sass()) //    Скомпилируем
+        .pipe(prefixer()) //    Добавим вендорные префиксы
+        .pipe(cssmin()) //  Сожмем
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest("dist/css"))
-        //.pipe(browserSync.stream());
+        .pipe(gulp.dest('dist/css'))
+        // .pipe(browserSync.stream());
         .pipe(reload({stream: true}));
 });
 
 gulp.task('sass:build', function() {
     return gulp.src(['src/sass/*.sass']) // Выбираем наши scss фаилы
-        .pipe(sourcemaps.init()) //То же самое что и с js
-        .pipe(sass()) //Скомпилируем
-        .pipe(prefixer()) //Добавим вендорные префиксы
-        .pipe(cssmin()) //Сожмем
+        .pipe(sourcemaps.init()) // То же самое что и с js
+        .pipe(sass()) // Скомпилируем
+        .pipe(prefixer()) // Добавим вендорные префиксы
+        .pipe(cssmin()) // Сожмем
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest("dist/css"))
+        .pipe(gulp.dest('dist/css'))
         .pipe(reload({stream: true}));
 });
 
 // Move JS Files to src/js
 gulp.task('js:build', function() {
-     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 
-                      'node_modules/jquery/dist/jquery.min.js',
-                      'node_modules/popper.js/dist/umd/popper.min.js',
-                      'src/js/*.js'])
-        .pipe(sourcemaps.init()) //Инициализируем sourcemap
-        .pipe(uglify()) //Сожмем наш js
-        .pipe(sourcemaps.write()) //Пропишем карты
-        .pipe(gulp.dest("dist/js"))
-        //.pipe(browserSync.stream());
+     return gulp.src([
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/popper.js/dist/umd/popper.min.js',
+        'src/js/*.js'
+    ])
+        .pipe(sourcemaps.init()) // Инициализируем sourcemap
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+        .pipe(uglify()) // Сожмем наш js
+        .pipe(sourcemaps.write()) // Пропишем карты
+        .pipe(gulp.dest('dist/js'))
+        // .pipe(browserSync.stream());
         .pipe(reload({stream: true}));
 });
 
-gulp.task('image:build', function () {
-    return gulp.src(['src/img/**/*.*']) //Выберем наши картинки
-        .pipe(imagemin({ //Сожмем их
+gulp.task('image:build', function() {
+    return gulp.src(['src/img/**/*.*']) // Выберем наши картинки
+        .pipe(imagemin({ // Сожмем их
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()],
             interlaced: true
         }))
-        .pipe(gulp.dest('dist/img')) //И бросим в build
-        //.pipe(browserSync.stream());
+        .pipe(gulp.dest('dist/img')) // И бросим в build
+        // .pipe(browserSync.stream());
         .pipe(reload({stream: true}));
 });
 
-gulp.task('html:build', function () {
-    return gulp.src('src/*.html') //Выберем файлы по нужному пути
-        .pipe(gulp.dest('dist/')) //Выплюнем их в папку build
-        //.pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
-        .pipe(reload({stream: true}));
+gulp.task('html:build', function() {
+    return gulp.src('src/*.html')// Выберем файлы по нужному пути
+        .pipe(gulp.dest('dist/'))// Выплюнем их в папку build
+        .pipe(reload({stream: true}));// И перезагрузим наш сервер
 });
 
 // Move Fonts to src/fonts
 gulp.task('fonts:build', function() {
-  return gulp.src(['node_modules/font-awesome/fonts/*','src/fonts/**/*.*'])
+  return gulp.src([
+    'node_modules/font-awesome/fonts/*',
+    'src/fonts/**/*.*'
+])
     .pipe(gulp.dest('dist/fonts'))
     .pipe(reload({stream: true}));
 })
@@ -96,42 +105,40 @@ gulp.task('build', [
     'image:build'
 ]);
 
-gulp.task('webserver', function () {
+gulp.task('webserver', function() {
     browserSync({
         server: {
-            baseDir: "./dist"
+            baseDir: './dist'
         },
         tunnel: false,
         host: 'localhost',
         port: 8081,
-        logPrefix: "Asset Saparov"       
+        logPrefix: 'Asset Saparov'
     });
 });
 
-gulp.task('watch', function(){
-    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], function(event,cb){
+gulp.task('watch', function() {
+    gulp.watch([
+        'node_modules/bootstrap/scss/bootstrap.scss',
+        'src/scss/*.scss'], function(event, cb) {
         gulp.start(['sass:build']);
     });
-    gulp.watch(['src/js/*.js'],  function(event,cb){
+    gulp.watch(['src/js/*.js'], function(event, cb) {
         gulp.start(['js:build']);
     });
-    gulp.watch(["src/*.html"],function(event,cb){
+    gulp.watch(['src/*.html'], function(event, cb) {
         gulp.start(['html:build']);
     });
-    gulp.watch(["srct/img/*.*"],function(event,cb){
+    gulp.watch(['srct/img/*.*'], function(event, cb) {
         gulp.start(['image:build']);
     });
-    gulp.watch(["src/fonts/*.*"],function(event,cb){
+    gulp.watch(['src/fonts/*.*'], function(event, cb) {
         gulp.start(['fonts:build']);
     });
 });
 
-gulp.task('clean', function (cb) {
+gulp.task('clean', function(cb) {
     rimraf('./dist', cb);
 });
-
-
-
-
 
 gulp.task('default', ['build', 'webserver', 'watch']);
